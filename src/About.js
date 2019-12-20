fa368fb5e495618902d42608545d2ca777651811
BigbrotherShin <shinjhhp5@gmail.com>
2019-12-20 17:04:42 +0900

    browser brouter

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>Introduction</h1>
+      <p>
+        This project is a tutorial page to train react router.
+      </p>
+    </div>
+  )
+}
+
+export default About;
