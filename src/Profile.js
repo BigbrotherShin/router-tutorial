d3916e2c04598eaca1ebf2ebf4a95da4cd853dc0
BigbrotherShin <shinjhhp5@gmail.com>
2019-12-23 18:57:23 +0900

    sub-router, history and with-router 7

--- src/Profile.js
+++ src/Profile.js
@@ -7,26 +7,30 @@ const profileData = {
   },
   homer: {
     name: 'Simpson',
-    description: 'animation character'
-  }
-}
+    description: 'animation character',
+  },
+};
 
 function Profile({ match }) {
-  const { username } = match.params; // url parameter or query 읽어오기
+  // 파라미터를 받아올 땐 match 안에 들어있는 params 값을 참조
+  const { username } = match.params; 
+  // Profile 을 Profiles 에서 적용: path 규칙에는 /profiles/:username 이라고 넣어주면 ,
+  // username 에 해당하는 값을 파라미터로 넣어주어서 Profile 컴포넌트에서 match props 를 통하여 전달받을 수 있게 됨 
+  // match 객체안에는 현재의 주소가 Route 컴포넌트에서 정한 규칙과 어떻게 일치하는지에 대한 정보가 들어있습니다.
   const profile = profileData[username];
 
   if (!profile) {
-    return <div>Cannot find user.</div>
+    return <div>Cannot find user.</div>;
   }
 
   return (
     <div>
-      <h3>{username} ({profile.name})</h3>
-      <p>
-        {profile.description}
-      </p>
+      <h3>
+        {username} ({profile.name})
+      </h3>
+      <p>{profile.description}</p>
     </div>
-  )
+  );
 }
 
 export default Profile;
